84b9d600b160ca8773fce91804c0813244877b17
shintumintu <mintushintu41@gmail.com>
2020-07-12 17:41:32 +0530

    First Commit

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,10 +1,13 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:glowchat/constants/user.dart';
-
 
 class FirestoreService {
   final CollectionReference _usersCollectionReference =
       Firestore.instance.collection('users');
+  final StreamController<List<User>> _postsController =
+      StreamController<List<User>>.broadcast();
 
   Future createUser(User user) async {
     try {
@@ -21,5 +24,20 @@ class FirestoreService {
     } catch (e) {
       return e.message;
     }
+  }
+
+  Stream listenToPOstREALtime() {
+    _usersCollectionReference.snapshots().listen((event) {
+      if (event.documents.isNotEmpty) {
+        var userdetails = event.documents
+            .map((snapshot) => User.fromData(
+                  snapshot.data,
+                ))
+            .where((element) => element.fullname != null)
+            .toList();
+        _postsController.add(userdetails);
+      }
+    });
+    return _postsController.stream;
   }
 }
