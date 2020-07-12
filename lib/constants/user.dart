03d8f2abdb6985ab09e6e9410359c9857be1ac0a
shintumintu <mintushintu41@gmail.com>
2020-07-12 16:54:30 +0530

    First Commit

--- lib/constants/user.dart
+++ lib/constants/user.dart
@@ -2,19 +2,38 @@ class User {
   final String id;
   final String fullname;
   final String email;
+  final String userimage;
+  final String phonenumber;
+  final String bio;
+  final String userRole;
 
-  User({this.email,this.id ,this.fullname});
+  User(
+      {this.email,
+      this.id,
+      this.fullname,
+      this.userimage,
+      this.phonenumber,
+      this.bio,
+      this.userRole});
 
   User.fromData(Map<String, dynamic> data)
       : email = data['email'],
         id = data['id'],
-        fullname = data['fullname'];
+        fullname = data['fullname'],
+        userimage = data['userimage'],
+        phonenumber = data['phonenumber'],
+        bio = data['bio'],
+        userRole = data['userRole'];
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'name': fullname,
-      'email': email, 
+      'email': email,
+      'userimage': userimage,
+      'phonenumber': phonenumber,
+      'bio': bio,
+      'userRole': userRole,
     };
   }
 }
